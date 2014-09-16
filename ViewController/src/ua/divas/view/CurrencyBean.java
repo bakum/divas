2c03e61520814f851d4cb4c7b55ac2cac2cebaff
Bakum Viacheslav <bakum.v@gmail.com>
2014-09-16 20:02:55 +0300

    deception point before DnD on divisions

--- ViewController/src/ua/divas/view/CurrencyBean.java
+++ ViewController/src/ua/divas/view/CurrencyBean.java
@@ -9,11 +9,18 @@ import javax.faces.event.ActionEvent;
 import oracle.adf.model.BindingContext;
 import oracle.adf.model.binding.DCBindingContainer;
 import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.view.rich.component.rich.RichPopup;
 import oracle.adf.view.rich.component.rich.data.RichTable;
 
 import oracle.adf.view.rich.component.rich.data.RichTree;
 
 import oracle.adf.view.rich.context.AdfFacesContext;
+
+import oracle.adf.view.rich.dnd.DnDAction;
+import oracle.adf.view.rich.event.DialogEvent;
+import oracle.adf.view.rich.event.DropEvent;
+import oracle.adf.view.rich.event.PopupCanceledEvent;
+import oracle.adf.view.rich.event.PopupFetchEvent;
 
 import oracle.binding.BindingContainer;
 import oracle.binding.OperationBinding;
@@ -30,8 +37,17 @@ import org.apache.myfaces.trinidad.model.RowKeySet;
 public class CurrencyBean {
     private RichTable mainTable;
     private RichTree mainTree;
+    private RichPopup mainPopup;
 
     public CurrencyBean() {
+    }
+
+    public void setMainPopup(RichPopup mainPopup) {
+        this.mainPopup = mainPopup;
+    }
+
+    public RichPopup getMainPopup() {
+        return mainPopup;
     }
 
     public void setMainTable(RichTable mainTable) {
@@ -41,7 +57,15 @@ public class CurrencyBean {
     public RichTable getMainTable() {
         return mainTable;
     }
-    
+
+    public void setMainTree(RichTree mainTree) {
+        this.mainTree = mainTree;
+    }
+
+    public RichTree getMainTree() {
+        return mainTree;
+    }
+
     public void rowDiscloseListener(RowDisclosureEvent rowDisclosureEvent) {
         RowKeySet addedSet = rowDisclosureEvent.getAddedSet();
         Object object = rowDisclosureEvent.getSource();
@@ -52,7 +76,7 @@ public class CurrencyBean {
             this.makeDisclosedRowCurrent(rowDisclosureEvent, (Key) rowKeys.get(0));
         }
     }
-    
+
     /**
      * Synchronizes the table UI component row selection with the
      * selection in the ADF binding layer
@@ -78,7 +102,7 @@ public class CurrencyBean {
         Key _rwKey = key;
         _tableIteratorBinding.setCurrentRowWithKey(_rwKey.toStringFormat(true));
     }
-    
+
     public void refresh() {
         DCBindingContainer binding = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
         DCIteratorBinding it = binding.findIteratorBinding("CurrencyView1Iterator");
@@ -119,39 +143,69 @@ public class CurrencyBean {
         refresh();
         return null;
     }
-    
+
     public BindingContainer getBindings() {
-            return BindingContext.getCurrent().getCurrentBindingsEntry();
-        }
-    
+        return BindingContext.getCurrent().getCurrentBindingsEntry();
+    }
+
     public String doExRates() {
-            FacesContext ctx = FacesContext.getCurrentInstance();
-            OperationBinding operationBinding = getBindings().getOperationBinding("doCurrencyExchange");
-            operationBinding.execute();
-            if (!operationBinding.getErrors().isEmpty()) {
-                FacesMessage msg =
-                    new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error",
-                                     "Произошла ошибка при загрузке курсов!");
-                ctx.addMessage(null, msg);
-                return null;
-            }
-            
+        FacesContext ctx = FacesContext.getCurrentInstance();
+        OperationBinding operationBinding = getBindings().getOperationBinding("doCurrencyExchange");
+        operationBinding.execute();
+        if (!operationBinding.getErrors().isEmpty()) {
             FacesMessage msg =
-                new FacesMessage(FacesMessage.SEVERITY_INFO, "Infirmation",
-                                 "Загрузка курсов завершена!");
+                new FacesMessage(FacesMessage.SEVERITY_ERROR, "Error", "Произошла ошибка при загрузке курсов!");
             ctx.addMessage(null, msg);
             return null;
         }
 
-    public void setMainTree(RichTree mainTree) {
-        this.mainTree = mainTree;
+        FacesMessage msg = new FacesMessage(FacesMessage.SEVERITY_INFO, "Infirmation", "Загрузка курсов завершена!");
+        ctx.addMessage(null, msg);
+        return null;
     }
 
-    public RichTree getMainTree() {
-        return mainTree;
-    }
 
     public void returnListener(ReturnEvent returnEvent) {
         AdfFacesContext.getCurrentInstance().addPartialTarget(mainTree.getParent());
+    }
+
+    public void onPopup(ActionEvent actionEvent) {
+        RichPopup.PopupHints hints = new RichPopup.PopupHints();
+        getMainPopup().show(hints);
+    }
+
+
+    public void fetchListener(PopupFetchEvent popupFetchEvent) {
+        if (popupFetchEvent.getLaunchSourceClientId() == null) {
+            return;
+        }
+        if (popupFetchEvent.getLaunchSourceClientId().contains("cbInsertDiv")) {
+            BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+            OperationBinding ob = binding.getOperationBinding("CreateInsert2");
+            ob.execute();
+        }
+    }
+
+    public void cancelListener(PopupCanceledEvent popupCanceledEvent) {
+        BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+        OperationBinding ob = binding.getOperationBinding("Rollback");
+        ob.execute();
+    }
+
+    public void dialogListener(DialogEvent dialogEvent) {
+        if (dialogEvent.getOutcome().name().equals("ok")) {
+            BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+            OperationBinding ob = binding.getOperationBinding("Commit");
+            ob.execute();
+        } else if (dialogEvent.getOutcome().name().equals("cancel")) {
+            BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+            OperationBinding ob = binding.getOperationBinding("Rollback");
+            ob.execute();
+        }
+    }
+
+    public DnDAction dropHundler(DropEvent dropEvent) {
+        // Add event code here...
+        return null;
     }
 }
