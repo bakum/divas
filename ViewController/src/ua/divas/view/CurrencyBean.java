1b7db1300473f2cb6b7d54f4365c3efa12124acd
Bakum Viacheslav <bakum.v@gmail.com>
2014-09-16 22:05:36 +0300

    DnD divisions complete

--- ViewController/src/ua/divas/view/CurrencyBean.java
+++ ViewController/src/ua/divas/view/CurrencyBean.java
@@ -1,5 +1,7 @@
 package ua.divas.view;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import javax.faces.application.FacesMessage;
@@ -16,6 +18,7 @@ import oracle.adf.view.rich.component.rich.data.RichTree;
 
 import oracle.adf.view.rich.context.AdfFacesContext;
 
+import oracle.adf.view.rich.datatransfer.DataFlavor;
 import oracle.adf.view.rich.dnd.DnDAction;
 import oracle.adf.view.rich.event.DialogEvent;
 import oracle.adf.view.rich.event.DropEvent;
@@ -27,12 +30,16 @@ import oracle.binding.OperationBinding;
 
 import oracle.jbo.Key;
 
+import oracle.jbo.Row;
 import oracle.jbo.uicli.binding.JUCtrlHierBinding;
+
+import oracle.jbo.uicli.binding.JUCtrlHierNodeBinding;
 
 import org.apache.myfaces.trinidad.event.ReturnEvent;
 import org.apache.myfaces.trinidad.event.RowDisclosureEvent;
 import org.apache.myfaces.trinidad.model.CollectionModel;
 import org.apache.myfaces.trinidad.model.RowKeySet;
+import org.apache.myfaces.trinidad.model.RowKeySetImpl;
 
 public class CurrencyBean {
     private RichTable mainTable;
@@ -205,7 +212,87 @@ public class CurrencyBean {
     }
 
     public DnDAction dropHundler(DropEvent dropEvent) {
-        // Add event code here...
-        return null;
+        RichTree tree1 = (RichTree) dropEvent.getDragComponent();
+        RichTree tree = (RichTree) dropEvent.getDropComponent();
+        RowKeySet droppedValue = null;
+        String Id = null;
+        Iterator it = null;
+        Object currentRowKey = tree.getRowKey();
+        List dropRowKey = (List) dropEvent.getDropSite();
+        if (dropRowKey == null) {
+            return DnDAction.NONE;
+        }
+        
+        try {
+            DataFlavor<RowKeySet> df = DataFlavor.getDataFlavor(RowKeySet.class, "rowmove");
+            droppedValue = dropEvent.getTransferable().getData(df);
+            if (droppedValue != null) {
+                it = droppedValue.iterator();
+                if (it.hasNext()) {
+                    List key = (List) it.next();
+                    tree1.setRowKey(key);
+                    JUCtrlHierNodeBinding rowBinding = (JUCtrlHierNodeBinding) tree1.getRowData();
+                    Row tableRow = (Row) rowBinding.getRow();
+                    Id = (String) tableRow.getAttribute("Id");
+
+                    tree.setRowKey(dropRowKey);
+                    JUCtrlHierNodeBinding dropNode = (JUCtrlHierNodeBinding) tree.getRowData();
+                    //Row treeRow = dropNode.getRow();
+                    String dropNodeVO = dropNode.getHierTypeBinding().getStructureDefName();
+                    if (!dropNodeVO.equalsIgnoreCase("ua.divas.view.DivisionsView")) {
+                        return DnDAction.NONE;
+                    }
+
+                    tree.setRowKey(currentRowKey);
+                    CollectionModel treeModel = (CollectionModel) tree.getValue();
+                    JUCtrlHierBinding treeBinding = (JUCtrlHierBinding) treeModel.getWrappedData();
+
+                    JUCtrlHierNodeBinding treeDropNode = treeBinding.findNodeByKeyPath(dropRowKey);
+                    JUCtrlHierNodeBinding rootNode = treeBinding.getRootNodeBinding();
+                    JUCtrlHierNodeBinding dropNodeParent = treeDropNode.getParent();
+
+                    //walk up the tree to expand all parent nodes
+                    if (treeDropNode != null && treeDropNode != rootNode) {
+                        /*RowKeySet drk = tree.getDisclosedRowKeys();
+                         if (drk != null) {
+                            drk.clear();
+                            AdfFacesContext.getCurrentInstance().addPartialTarget(tree);
+                        } */
+                        RowKeySetImpl rksImpl = new RowKeySetImpl();
+                        rksImpl.add(dropRowKey);
+                        String ParentId = (String) treeDropNode.getRow().getAttribute("Id");
+                        BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+                         OperationBinding oper =
+                            (OperationBinding) binding.getOperationBinding("changeParentInDivisions");
+                        oper.getParamsMap().put("Id", Id);
+                        oper.getParamsMap().put("ParentId", ParentId);
+                        oper.execute();
+                        //walk up the tree to expand all parent nodes
+                        while (dropNodeParent != null && dropNodeParent != rootNode) {
+                            rksImpl.add(dropNodeParent.getKeyPath());
+                            dropNodeParent = dropNodeParent.getParent();
+                        }
+                        //3. get all employee nodes in a tree to disclose them too
+                        ArrayList<JUCtrlHierNodeBinding> childList =
+                            (ArrayList<JUCtrlHierNodeBinding>) treeDropNode.getChildren();
+                        for (JUCtrlHierNodeBinding nb : childList) {
+                            rksImpl.add(nb.getKeyPath());
+                        }
+                        //ready to disclose
+                        tree.setDisclosedRowKeys(rksImpl);
+                        AdfFacesContext.getCurrentInstance().addPartialTarget(tree.getParent());
+                        AdfFacesContext.getCurrentInstance().addPartialTarget(tree1);
+
+                    } else {
+                        return DnDAction.NONE;
+                    }
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            return DnDAction.NONE;
+        }
+        return DnDAction.MOVE;
     }
 }
