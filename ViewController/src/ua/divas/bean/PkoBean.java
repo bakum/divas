3c0caa9d63506b89d03a7893b9ed12eda6ed0e96
Bakum Viacheslav <bakum.v@gmail.com>
2015-04-16 12:43:27 +0300

    РКО ПКО улучшаем

--- ViewController/src/ua/divas/bean/PkoBean.java
+++ ViewController/src/ua/divas/bean/PkoBean.java
@@ -2,21 +2,18 @@ package ua.divas.bean;
 
 import oracle.adf.model.BindingContext;
 import oracle.adf.model.binding.DCBindingContainer;
-
 import oracle.adf.model.binding.DCIteratorBinding;
-
 import oracle.adf.view.rich.component.rich.data.RichTable;
-
 import oracle.adf.view.rich.context.AdfFacesContext;
 
 import org.apache.myfaces.trinidad.event.ReturnEvent;
 
-public class RkoBean {
+public class PkoBean {
     private RichTable table;
 
-    public RkoBean() {
+    public PkoBean() {
     }
-    
+
     public void setTable(RichTable table) {
         this.table = table;
     }
@@ -24,11 +21,11 @@ public class RkoBean {
     public RichTable getTable() {
         return table;
     }
-
+    
     public String refresh() {
 
         DCBindingContainer binding = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
-        DCIteratorBinding it = binding.findIteratorBinding("RkoView1Iterator");
+        DCIteratorBinding it = binding.findIteratorBinding("PkoView1Iterator");
         String rks;
         if (it != null) {
             try {
@@ -50,7 +47,7 @@ public class RkoBean {
     }
 
     public void onReturn(ReturnEvent re) {
-        System.out.println(re.getReturnParameters().get("RkoId")+" Return listener called ");
+        System.out.println(re.getReturnParameters().get("PkoId")+" Return listener called ");
         AdfFacesContext.getCurrentInstance().addPartialTarget(getTable());
         refresh();
     }
