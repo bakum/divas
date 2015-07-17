8603977b3844804b79f64421374b8dad4e366c30
Bakum Viacheslav <bakum.v@gmail.com>
2015-07-17 12:14:54 +0300

    zatraty edit added

---
+++ ViewController/src/ua/divas/bean/ZatratyBean.java
@@ -0,0 +1,64 @@
+package ua.divas.bean;
+
+import javax.faces.event.ActionEvent;
+
+import oracle.adf.model.BindingContext;
+import oracle.adf.model.binding.DCBindingContainer;
+import oracle.adf.model.binding.DCIteratorBinding;
+import oracle.adf.view.rich.component.rich.data.RichTable;
+import oracle.adf.view.rich.context.AdfFacesContext;
+
+import oracle.binding.BindingContainer;
+import oracle.binding.OperationBinding;
+
+public class ZatratyBean {
+    private RichTable table;
+
+    public ZatratyBean() {
+    }
+
+    public void setTable(RichTable table) {
+        this.table = table;
+    }
+
+    public RichTable getTable() {
+        return table;
+    }
+    
+    public void refresh() {
+        DCBindingContainer binding = (DCBindingContainer) BindingContext.getCurrent().getCurrentBindingsEntry();
+        DCIteratorBinding it = binding.findIteratorBinding("ZatratyView1Iterator");
+        String rks;
+        if (it != null) {
+            try {
+                rks = it.getCurrentRow().getKey().toStringFormat(true);
+            } catch (Exception e) {
+                rks = null;
+            }
+            it.executeQuery();
+            if (rks != null) {
+                try {
+                    it.setCurrentRowWithKey(rks);
+                } catch (Exception e) {
+                    e.getMessage();
+                }
+            }
+        }
+        
+        AdfFacesContext.getCurrentInstance().addPartialTarget(getTable());
+    }
+    
+    public String commitChange() {
+        //this.setSupplierParentId();
+        //this.setIsSupplier();
+        BindingContainer binding = BindingContext.getCurrent().getCurrentBindingsEntry();
+        OperationBinding ob = binding.getOperationBinding("Commit");
+        ob.execute();
+        refresh();
+        return null;
+    }
+
+    public void onRefresh(ActionEvent actionEvent) {
+        refresh();
+    }
+}
