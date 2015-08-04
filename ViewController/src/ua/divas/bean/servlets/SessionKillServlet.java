045c9135beaa6a43169113b9b489b346b8ebae8d
Bakum Viacheslav <bakum.v@gmail.com>
2015-08-04 12:45:40 +0300

    sessionkillServlet

---
+++ ViewController/src/ua/divas/bean/servlets/SessionKillServlet.java
@@ -0,0 +1,41 @@
+package ua.divas.bean.servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+
+@WebServlet(name = "SessionKillServlet", urlPatterns = { "/sessionkillservlet" })
+public class SessionKillServlet extends HttpServlet {
+    @SuppressWarnings("compatibility:-1015554902259394130")
+    private static final long serialVersionUID = 1L;
+    private static final String CONTENT_TYPE = "text/html; charset=UTF-8";
+
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>SessionKillServlet</title></head>");
+        out.println("<body>");
+        out.println("<p>The servlet has received a GET. This is the reply.</p>");
+        out.println("</body></html>");
+        out.close();
+    }
+
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType(CONTENT_TYPE);
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head><title>SessionKillServlet</title></head>");
+        out.println("<body>");
+        out.println("<p>The servlet has received a POST. This is the reply.</p>");
+        out.println("</body></html>");
+        out.close();
+    }
+}
